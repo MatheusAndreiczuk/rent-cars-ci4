a9aa02050dcfef0400de22152eeefc6c8d72a693
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-08 12:47:15 -0300

    feat: adicionar modal de usuario com regras de validação

---
+++ app/Models/UserModel.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model {
+    protected $useTimestamps = true;
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $useSoftDeletes = true;
+
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+    protected $allowedFields = [
+        'nome',
+        'cpf',
+        'email',
+        'telefone',
+        'cnh_numero',
+        'cnh_validade',
+        'cnh_categoria',
+        'cep',
+        'numero',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+    protected $validationRules = [
+        'nome' => 'required|min_length[3]',
+        'cpf' => 'required|is_unique[users.cpf]|exact_length[11]',
+        'email' => 'required|valid_email|is_unique[users.email]',
+        'telefone' => 'required|min_length[10]|max_length[15]',
+        'cnh_numero' => 'required|is_unique[users.cnh_numero]',
+        'cnh_validade' => 'required',
+        'cnh_categoria' => 'required',
+        'cep' => 'required|exact_length[8]',
+        'numero' => 'required'
+    ];
+}
