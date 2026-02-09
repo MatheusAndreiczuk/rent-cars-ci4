8e7318bae63c405a553d1925230d71a34b30c59e
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-09 18:04:59 -0300

    refactor: substituir rotas individuais de veículos por resource do ci4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,9 +11,5 @@ $routes->resource('users', ['controller' => 'UserController']);
 
 $routes->resource('categories', ['controller' => 'CategoryController']);
 
-$routes->post('/vehicles', 'VehicleController::create');
-$routes->get('/vehicles/(:num)', 'VehicleController::getVehicle/$1');
-$routes->get('/vehicles', 'VehicleController::getAllVehicles');
-$routes->put('/vehicles/(:num)', 'VehicleController::update/$1');
-$routes->delete('/vehicles/(:num)', 'VehicleController::delete/$1');
+$routes->resource('vehicles', ['controller' => 'VehicleController']);
 
