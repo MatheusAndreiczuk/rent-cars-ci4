354b5f8cfc375a79531b7c9550e1be4b52cd2ae7
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-09 00:02:24 -0300

    feat: adicionar rotas de usuario em Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('/users', 'UserController::create');
+$routes->get('/users/(:num)', 'UserController::getUser/$1');
+$routes->put('/users/(:num)', 'UserController::update/$1');
+$routes->delete('/users/(:num)', 'UserController::delete/$1');
+$routes->get('/users', 'UserController::getAllUsers');
