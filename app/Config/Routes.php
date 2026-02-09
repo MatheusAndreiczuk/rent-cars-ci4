7680b4931326d2c07ec81ff8005ba07e2e00e85b
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-09 14:59:32 -0300

    feat: adicionar rotas CRUD para categorias em Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,10 @@ $routes->get('/users/(:num)', 'UserController::getUser/$1');
 $routes->put('/users/(:num)', 'UserController::update/$1');
 $routes->delete('/users/(:num)', 'UserController::delete/$1');
 $routes->get('/users', 'UserController::getAllUsers');
+
+$routes->post('/categories', 'CategoryController::create');
+$routes->get('/categories/(:num)', 'CategoryController::getCategory/$1');
+$routes->get('/categories', 'CategoryController::getAllCategories');
+$routes->put('/categories/(:num)', 'CategoryController::update/$1');
+$routes->delete('/categories/(:num)', 'CategoryController::delete/$1');
+
