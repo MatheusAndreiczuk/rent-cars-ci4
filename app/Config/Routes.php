86ad80057080b29d91a78c07e0c8c09a9663b630
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-09 17:54:05 -0300

    refactor: simplificar rotas utilizando resource do ci4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,17 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->post('/users', 'UserController::create');
-$routes->get('/users/(:num)', 'UserController::getUser/$1');
-$routes->put('/users/(:num)', 'UserController::update/$1');
-$routes->delete('/users/(:num)', 'UserController::delete/$1');
-$routes->get('/users', 'UserController::getAllUsers');
 
-$routes->post('/categories', 'CategoryController::create');
-$routes->get('/categories/(:num)', 'CategoryController::getCategory/$1');
-$routes->get('/categories', 'CategoryController::getAllCategories');
-$routes->put('/categories/(:num)', 'CategoryController::update/$1');
-$routes->delete('/categories/(:num)', 'CategoryController::delete/$1');
+$routes->resource('users', ['controller' => 'UserController']);
+
+$routes->resource('categories', ['controller' => 'CategoryController']);
 
 $routes->post('/vehicles', 'VehicleController::create');
 $routes->get('/vehicles/(:num)', 'VehicleController::getVehicle/$1');
