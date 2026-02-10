62cd9110991cd25335755a2b19729f4980dfe263
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-10 17:51:22 -0300

    feat: adicionar método hash_password e atributos role e senha no UserModel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -17,6 +17,8 @@ class UserModel extends Model {
         'nome',
         'cpf',
         'email',
+        'senha',
+        'role',
         'telefone',
         'cnh_numero',
         'cnh_validade',
@@ -30,7 +32,9 @@ class UserModel extends Model {
     protected $validationRules = [
         'nome' => 'required|min_length[3]',
         'cpf' => 'required|is_unique[users.cpf]|exact_length[11]',
+        'senha' => 'required|min_length[6]',
         'email' => 'required|valid_email|is_unique[users.email]',
+        'role' => 'required',
         'telefone' => 'required|min_length[10]|max_length[15]',
         'cnh_numero' => 'required|is_unique[users.cnh_numero]',
         'cnh_validade' => 'required',
@@ -39,12 +43,29 @@ class UserModel extends Model {
         'numero' => 'required'
     ];
 
+    protected $beforeInsert = ['hashPassword'];
+    protected $beforeUpdate = ['hashPassword'];
+
+    protected function hashPassword(array $data)
+    {
+        if (!empty($data['data']['senha'])) {
+            $data['data']['senha'] = password_hash($data['data']['senha'], PASSWORD_DEFAULT);
+        } else {
+            unset($data['data']['senha']);
+        }
+        return $data;
+    }
+
     public function updateUser($id, $data)
     {
         $rules = $this->validationRules;
         $rules['cpf']        = "required|exact_length[11]|is_unique[users.cpf,id,{$id}]";
         $rules['email']      = "required|valid_email|is_unique[users.email,id,{$id}]";
         $rules['cnh_numero'] = "required|is_unique[users.cnh_numero,id,{$id}]";
+
+        if (empty($data['senha'])) {
+            unset($rules['senha']);
+        }
 
         $this->setValidationRules($rules);
 
