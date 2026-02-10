0f242c3d7ee3796839a453f575f8d369d6cf6fdb
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2026-02-10 14:40:11 -0300

    feat: adicionar rotas para o RentalController

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,3 +13,12 @@ $routes->resource('categories', ['controller' => 'CategoryController']);
 
 $routes->resource('vehicles', ['controller' => 'VehicleController']);
 
+// rentals 
+$routes->get('rentals', 'RentalController::index');
+$routes->post('rentals', 'RentalController::create');               
+$routes->get('rentals/(:num)', 'RentalController::show/$1');
+$routes->delete('rentals/(:num)', 'RentalController::delete/$1');
+$routes->put('rentals/(:num)/start', 'RentalController::start/$1');      // retirada
+$routes->put('rentals/(:num)/finish', 'RentalController::finish/$1');    // devolução
+$routes->put('rentals/(:num)/cancel', 'RentalController::cancel/$1');    // cancelamento
+
